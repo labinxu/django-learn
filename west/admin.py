bf551762038cf607be9242536ea26cb2881e9501
labin.xu <labin@localhost.localdomain>
2015-12-17 19:44:43 +0800

    lessen5 model show

--- west/admin.py
+++ west/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from west.models import Character, Contact, Tag, ContactAdmin
+
+admin.site.register([Character, Tag])
+admin.site.register(Contact, ContactAdmin)
--- west/models.py
+++ west/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django import forms
-
+from django.contrib import admin
 
 # Create your models here.
 
@@ -9,6 +9,36 @@ class Character(models.Model):
     def __str__(self):
         return self.name
 
+class Contact(models.Model):
+    name = models.CharField(max_length=200)
+    age = models.IntegerField(default=0)
+    email = models.EmailField()
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    contact = models.ForeignKey(Contact)
+    name = models.IntegerField(default=0)
+    def __str__(self):
+        return self.name
+
+class TagInline(admin.TabularInline):
+    model = Tag
+
+class ContactAdmin(admin.ModelAdmin):
+    inlines = [TagInline]
+    fieldsets = (
+        ['Main',
+         {'fields':('name','email')}],
+
+        ['Advance',{
+            'classes':('collapse',),
+            'fields':('age',)}
+        ]
+    )
+
+    list_display = ('name','age', 'email')
+    search_fields =('name','age')
 
 class CharacterForm(forms.Form):
     name = forms.CharField(max_length=200)
